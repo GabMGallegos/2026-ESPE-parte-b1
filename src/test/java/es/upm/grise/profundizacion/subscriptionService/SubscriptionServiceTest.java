6fe4ebc5fa821b424e5a5bd33fe8446237c62398
GabMGallegos <gabriel25molina@hotmail.com>
2026-01-22 08:52:42 -0500

    Creacion de los Test respectivps

--- src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,10 +1,45 @@
 package es.upm.grise.profundizacion.subscriptionService;
+import es.upm.grise.profundizacion.exceptions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 public class SubscriptionServiceTest {
 	
 	@Test
-	public void smokeTest() {}
+	public void subscriptionService_seCreaElObjetoNormalmente() {
+		SubscriptionService service = new SubscriptionService();
+
+		assertNotNull(service);
+	}
+
+	@Test
+	public void addUser_colocarUnUsuarioNulo(){
+		SubscriptionService service = new SubscriptionService();
+		User user = new User();
+		user.setEmail(null);
+		user.setDelivery(null);
+
+		assertThrowsExactly(NullUserException.class, () -> service.addSubscriber(user));
+	}
+
+	@Test
+	public void addUser_colocarUnUsuarioExistente(){
+		SubscriptionService service = new SubscriptionService();
+		User user = new User();
+		user.setEmail(null);
+		user.setDelivery(null);
+
+
+		assertThrows(NullUserException.class, ()-> service.addSubscriber(user));
+	}
 	
+	@Test
+	public void addUser_siElUsuarioEsLocalElEmailDebeSerNull(){
+		SubscriptionService service = new SubscriptionService();
+		User user = new User();
+		// user.setDelivery(LOCAl);
+
+	}
 }
